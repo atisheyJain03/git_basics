42f7ac47d7b74f0a96ab45b72d6e0a45a5440436
Atishey Jain <atisheyjain1232@gmail.com>
2020-05-18 18:38:16 +0530

    file01 greeter modified

--- file01.js
+++ file01.js
@@ -1,1 +1,7 @@
-console.log("file 01");
+let greet = (name) => {
+  console.log(`knock knock knock ${name}`);
+  console.log(`knock knock knock ${name}`);
+  console.log(`knock knock knock ${name}`);
+};
+
+greet("atishey");
